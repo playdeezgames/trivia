b993c04c7548df2f8c7ee3feece46736802de35e
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-09-09 08:10:59 -0500

    second player

--- C#/Trivia/Trivia.Tests/Game_should.cs
+++ C#/Trivia/Trivia.Tests/Game_should.cs
@@ -26,4 +26,23 @@ public class Game_should
         subject.IsPlayable().ShouldBeFalse();
         subject.HowManyPlayers().ShouldBe(1);
     }
+    [Fact]
+    public void add_two_players()
+    {
+        const string playerName1 = "yermom";
+        const string playerName2 = "nacho mama";
+        var log = new List<string>();
+        var subject = new Game(log.Add);
+
+        subject.Add(playerName1);
+        subject.Add(playerName2);
+
+        log.Count.ShouldBe(4);
+        log[0].ShouldContain($"{playerName1} was added");
+        log[1].ShouldContain("They are player number 1");
+        log[2].ShouldContain($"{playerName2} was added");
+        log[3].ShouldContain("They are player number 2");
+        subject.IsPlayable().ShouldBeTrue();
+        subject.HowManyPlayers().ShouldBe(2);
+    }
 }
